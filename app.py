ee535cf696daf8d8e0fc48ce3651f3010f3deddb
Lavanya <lavanyasaxena01012000@gmail.com>
2026-02-12 23:39:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-import joblib
-import os
+import os, joblib
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+# Paths
 model_path = os.path.join(BASE_DIR, "career_model.pkl")
 encoder_path = os.path.join(BASE_DIR, "label_encoder.pkl")
 columns_path = os.path.join(BASE_DIR, "model_columns.pkl")
 
+# Load
 model = joblib.load(model_path)
 label_encoder = joblib.load(encoder_path)
 model_columns = joblib.load(columns_path)
